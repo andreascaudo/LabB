b681ed4422cc11e2ca040ce13b6e2fc1d696efeb
Alessio <Alessio@Alessio-PC>
2017-09-02 18:52:29 +0200

    test

--- Server/src/ServerThread.java
+++ Server/src/ServerThread.java
@@ -286,7 +286,7 @@ public class ServerThread extends Thread {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
+            System.out.println("Connected   to the PostgreSQL server successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
